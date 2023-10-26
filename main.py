5e021c45aa72fec62546018e8cdaa13e3c2239da
jsg4hw <jsg4hw@virginia.edu>
2023-10-25 22:39:03 -0500

    Cleaned up main

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 import requests
-# from bs4 import BeautifulSoup
 import csv
 
-# city_ids = {
-#     'New York': 1
-#     # 'Chicago': 3,
-#     # 'Boston': 11,
-#     # 'DC': 6
-# }
-
-city_list = [
+city_list = [ #static list because I don't want all cities
     ('NewYork', 1),
     ('Chicago', 3),
     ('WashingtonDC', 6),
     ('Boston', 11)
 ]
 
-CITY_URL = "https://api.biltrewards.com/seated/cities/"
-
-# URL = "https://api.biltrewards.com/seated/restaurants?city_id=3&sort_by=rating"
 URL = "https://api.biltrewards.com/seated/restaurants?"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
+# saving for later to query the cities table rather than static list
 # cities = []
+# CITY_URL = "https://api.biltrewards.com/seated/cities/"
 # city_ping = requests.get(CITY_URL, headers=HEADERS)
 # city_ping.raise_for_status()
 # city_data = city_ping.json()
@@ -45,9 +31,6 @@ places = []
 for city, city_id in city_list:
 
     page = 0
-    # response0 = requests.get(f"{URL}&city_id={city_id}&sort_by=rating")
-    # data0 = response0.json()
-    # page = data0['meta_data']['page_number']
 
     while True:
 
@@ -55,11 +38,8 @@ for city, city_id in city_list:
         response.raise_for_status()
         data = response.json()
 
-        # print(data['meta_data']['page_number'])
-        # print(data['meta_data']['has_more_items'])
-
         for place in data['restaurants']:
-            # Extract the desired details. Again, the class names used below are placeholders and should be updated accordingly.
+            # Extract the restaurant details
             name = place['name']
             rating = place['rating']
             menu_url = place['menu_url']
@@ -70,30 +50,23 @@ for city, city_id in city_list:
             address = place['address']
             latitude = place['latitude']
             longitude = place['longitude']
-            coordinates = latitude, longitude
-            if primary_cuisine == ('Drinks' or 'Bars & Breweries'):
+            coordinates = latitude, longitude #using coordinates to avoid errors in uploading on MyMaps (address updates weren't working
+            if primary_cuisine == ('Drinks' or 'Bars & Breweries'): #create isBar variable to ease MyMaps labeling later
                 isBar = 'Bar'
             else:
                 isBar = 'Restaurant'
             points = place['multiplier']['monday']
-            # description = place['description']
 
-
-            # places.append([name, rating, menu_url, primary_cuisine, address, points, description])
             places.append([name, rating, menu_url, primary_cuisine, address, coordinates, points, isBar])
 
 
         if not data['meta_data']['has_more_items']: # checks the meta_data in the JSON to determine if there are more pages of data to query.
             break
-        page += 1        #If there are, it adds 1 to the page number
+        page += 1
 
 
     with open(f"{city}_restaurants.csv", 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        # writer.writerow(["Name", "Rating", "Menu Link", "Cuisine", "Address", "Points", "Description"])  # Header row.
         writer.writerow(["Name", "Rating", "Menu Link", "Cuisine", "Address", "Lat/Long", "Bilt Points", "Is Bar?"])  # Header row
         writer.writerows(places)
 
-# if __name__ == "__main__":
-#     restaurants = get_restaurants_info(URL)
-#     write_to_csv(restaurants)
